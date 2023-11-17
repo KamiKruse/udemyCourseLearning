dfce8a49fadb2af0a3225df89d5552cdb1673da8
Vivek Sreekumar <Enzen@Ulf-Hrafn.local>
2023-11-17 07:39:26 +0530

    Added Keyboard for sounds

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -3,14 +3,49 @@ let buttonLength = document.querySelectorAll(".drum")
 for(let i=0; i<buttonLength.length; i++)
 {
     buttonLength[i].addEventListener("click", function(){
-        // let audio = new Audio("sounds/tom-1.mp3")
-        // audio.play();
-        this.style.color = "white"
+       let pressedButton = this.innerHTML;
+       makeSound(pressedButton);
     });
 
 }   
-// let i=0;
 
-// document.querySelectorAll("button").addEventListener("click", function (){
-//     alert("I got clicked")
-// });
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+})
+
+function makeSound(key){
+
+    switch(key)
+       {
+            case "w":
+                let tom1 = new Audio("sounds/tom-1.mp3")
+                tom1.play();
+            break;
+            case "a":
+                let tom2 = new Audio("sounds/tom-2.mp3")
+                tom2.play();
+            break;
+            case "s":
+                let tom3 = new Audio("sounds/tom-3.mp3")
+                tom3.play();
+            break;
+            case "d":
+                let tom4 = new Audio("sounds/tom-4.mp3")
+                tom4.play();
+            break;
+            case "j":
+                let snare = new Audio("sounds/snare.mp3")
+                snare.play();
+            break;
+            case "k":
+                let crash = new Audio("sounds/crash.mp3")
+                crash.play();
+            break;
+            case "l":
+                let kick = new Audio("sounds/kick-bass.mp3")
+                kick.play();
+            break;
+            default:
+                console.log(pressedButton);
+       }
+}
